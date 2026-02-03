048406efd9b27ab99e1adaf2efb4fdf1ef7a4829
ShyLoveAdvice <paropbqiu@gmail.com>
2026-02-03 10:10:03 -0500

    Add Managers prefab and UI/utility scripts
    Introduce a new Managers.prefab consolidating manager objects (UIManager, GameManager, PlayerManager, PromptGenerator, RoundManager, SoundManager) to centralize runtime managers. Update Scene (Game.unity) to remove in-scene manager instances and add required widget/event entries. Add new UI scripts (Assets/Scripts/UI/MainMenuUI.cs, Assets/Scripts/UI/UIManager.cs), modify existing Assets/Scripts/UIManager.cs, and add a GameEvents utility (Assets/Scripts/Utilities/GameEvents.cs) along with corresponding .meta files.

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,44 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField CommandInputField;
+
+
+        private void OnEnable()
+        {
+            CommandInputField.onEndEdit.AddListener(OnCommandInputFieldEndEdit);
+        }
+
+        private void OnDisable()
+        {
+            CommandInputField.onEndEdit.RemoveListener(OnCommandInputFieldEndEdit);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            CommandInputField.Select();
+            CommandInputField.ActivateInputField();
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+        
+        private void OnCommandInputFieldEndEdit(string content)
+        {
+            if(content.ToLower() == UIManager.Instance.MainMenuCommandInputFieldEnterPlayKey)
+            {
+                UIManager.Instance.EnterPlay();
+            }
+        }
+
+        
+    }
+}
