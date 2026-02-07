dfe77013b2ced7b0400635b8096b9ece01b2b9c6
ShyLoveAdvice <paropbqiu@gmail.com>
2026-02-06 23:40:56 -0500

    fix

--- Assets/Scripts/UI/Client.cs
+++ Assets/Scripts/UI/Client.cs
@@ -362,7 +362,7 @@ public class Client : NetworkBehaviour
         return result;
     }
 
-    public const int k_winScore = 10;
+    public const int k_winScore = 30;
     //private const int k_maxScore = 75;
 
     private void OnCurrentScoreChanged(int prev, int value)
